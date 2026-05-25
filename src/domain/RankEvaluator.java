471ce11d333f5b814cc4c7cb2613f45957bb0e98
Thomas MAROTTA <thomasmarotta692@gmail.com>
2026-05-25 09:40:22 +0200

    Update hand evaluator

--- src/domain/RankEvaluator.java
+++ src/domain/RankEvaluator.java
@@ -7,84 +7,91 @@ import java.util.stream.IntStream;
 
 public class RankEvaluator {
 	public static EvaluatedHand evaluate(Hand hand) {
-	    HandRank rank = HandRank.HIGH_CARD;
 	    var rankGroup = groupByRank(hand.cards());
+	    var cards = hand.cards();
 
-	    if (isStraightFlush(hand)) {
-	        rank = HandRank.STRAIGHT_FLUSH;
-	    } else if (isFourOfAKind(rankGroup)) {
-	        rank = HandRank.FOUR_OF_A_KIND;
-	    } else if (isFullHouse(rankGroup)) {
-	        rank = HandRank.FULL_HOUSE;
-	    } else if (isFlush(hand)) {
-	        rank = HandRank.FLUSH;
-	    } else if (isStraight(hand)) {
-	        rank = HandRank.STRAIGHT;
-	    } else if (isThreeOfAKind(rankGroup)) {
-	        rank = HandRank.THREE_OF_A_KIND;
-	    } else if (isTwoPairs(rankGroup)) {
-	        rank = HandRank.TWO_PAIR;
-	    } else if (isPair(rankGroup)) {
-	        rank = HandRank.PAIR;
+
+        var straightFlushCards = getStraightFlushCards(hand);
+        if (!straightFlushCards.isEmpty()) {
+            return new EvaluatedHand(hand, HandRank.STRAIGHT_FLUSH, straightFlushCards);
+        }
+
+        var fourOfAKindCards = getByCount(rankGroup, cards, 4);
+        if (!fourOfAKindCards.isEmpty()) {
+            return new EvaluatedHand(hand, HandRank.FOUR_OF_A_KIND, fourOfAKindCards);
+        }
+
+        var fullHouseCards = getFullHouseCards(rankGroup, cards);
+        if (!fullHouseCards.isEmpty()) {
+            return new EvaluatedHand(hand, HandRank.FULL_HOUSE, fullHouseCards);
+        }
+
+        var flushCards = getFlushCards(hand);
+        if (!flushCards.isEmpty()) {
+            return new EvaluatedHand(hand, HandRank.FLUSH, flushCards);
+        }
+
+        var straightCards = getStraightCards(hand);
+        if (!straightCards.isEmpty()) {
+            return new EvaluatedHand(hand, HandRank.STRAIGHT, straightCards);
+        }
+
+        var threeOfAKindCards = getByCount(rankGroup, cards, 3);
+        if (!threeOfAKindCards.isEmpty()) {
+            return new EvaluatedHand(hand, HandRank.THREE_OF_A_KIND, threeOfAKindCards);
+        }
+	    
+        var twoPairCards = getTwoPairCards(rankGroup, cards);
+        if (!twoPairCards.isEmpty()) {
+            return new EvaluatedHand(hand, HandRank.TWO_PAIR, twoPairCards);
+        }
+        
+	    var pairCards = getByCount(rankGroup, cards, 2);
+	    if(!pairCards.isEmpty()) {
+	    	return new EvaluatedHand(hand, HandRank.PAIR, pairCards);
 	    }
 
-	    List<Card> activesCards = getActiveCards(hand, rank, rankGroup);
-
-	    return new EvaluatedHand(hand, rank, activesCards);
+	    return new EvaluatedHand(hand, HandRank.HIGH_CARD, cards);
 	}
 	
 	private static Map<Rank, Long> groupByRank(List<Card> cards) {
 	    return cards.stream().collect(Collectors.groupingBy(Card::rank, Collectors.counting()));
 	}
 	
-	private static boolean isPair(Map<Rank, Long> rankGroup) {
-		return rankGroup.values().stream().anyMatch(count -> count == 2L);
-	}
 	
-	private static boolean isTwoPairs(Map<Rank, Long> rankGroup) {
-		return rankGroup.values().stream().filter(count -> count == 2L).count() == 2L;
-	}
+    private static List<Card> getTwoPairCards(Map<Rank, Long> rankGroup, List<Card> cards) {
+        var isTwoPair = rankGroup.values().stream().filter(count -> count == 2L).count() == 2L;
+        return isTwoPair ? cards.stream().filter(c -> rankGroup.get(c.rank()) == 2L).toList() : List.of();
+    }
+	
+    private static List<Card> getStraightCards(Hand hand) {
+        var cards = hand.cards();
+        var orders = cards.stream().map(c -> c.rank().value()).sorted().toList();
+        
+        // Low card A-2-3-4-5 -> {2,3,4,5,14}
+        var isStraight = orders.equals(List.of(2, 3, 4, 5, 14))
+                || IntStream.range(1, orders.size()).allMatch(i -> orders.get(i) == orders.get(i - 1) + 1);
 
-	private static boolean isThreeOfAKind(Map<Rank, Long> rankGroup) {
-		return  rankGroup.values().stream().anyMatch(count -> count == 3L);
-	}
+        return isStraight ? cards : List.of();
+    }
 	
-	private static boolean isStraight(Hand hand) {
-		 var orders = hand.cards().stream().map(c -> c.rank().value()).sorted().toList();
-		 
-		// Low card A-2-3-4-5 -> {2,3,4,5,14}
-		 if(orders.equals(List.of(2, 3, 4, 5, 14))){
-			return true; 
-		 }
-		 
-		 return IntStream.range(1, orders.size()).allMatch(i -> orders.get(i) == orders.get(i - 1) + 1);
-	}
+
+    private static List<Card> getFlushCards(Hand hand) {
+        var cards = hand.cards();
+        return cards.stream().map(Card::suit).distinct().count() == 1 ? cards : List.of();
+    }
+
 	
-	private static boolean isFlush(Hand hand) {
-		return hand.cards().stream().map(Card::suit).distinct().count() == 1;
-	}
+    private static List<Card> getFullHouseCards(Map<Rank, Long> rankGroup, List<Card> cards) {
+        return rankGroup.values().containsAll(List.of(2L, 3L)) ? cards : List.of();
+    }
 	
-	private static boolean isFullHouse(Map<Rank, Long> rankGroup) {
-		return rankGroup.values().containsAll(List.of(2L, 3L));
-	}
-	
-	private static boolean isFourOfAKind(Map<Rank, Long> rankGroup) {
-		return  rankGroup.values().containsAll(List.of(1L, 4L));
-	}
-	
-	private static boolean isStraightFlush(Hand hand) {
-		return isStraight(hand) && isFlush(hand);
-	}
-	
-	private static List<Card> getActiveCards(Hand hand, HandRank rank, Map<Rank, Long> rankGroup) {
-	    return switch (rank) {
-	        case STRAIGHT_FLUSH, STRAIGHT, FLUSH, FULL_HOUSE, HIGH_CARD-> hand.cards();
-	        case FOUR_OF_A_KIND -> getByCount(rankGroup, hand.cards(), 4);
-	        case THREE_OF_A_KIND -> getByCount(rankGroup, hand.cards(), 3);
-	        case TWO_PAIR -> hand.cards().stream().filter(c -> rankGroup.get(c.rank()) == 2L).toList();
-	        case PAIR -> getByCount(rankGroup, hand.cards(), 2);
-	    };
-	}
+    private static List<Card> getStraightFlushCards(Hand hand) {
+        var straight = getStraightCards(hand);
+        var flush = getFlushCards(hand);
+        return (!straight.isEmpty() && !flush.isEmpty()) ? hand.cards() : List.of();
+    }
+
 
 	private static List<Card> getByCount(Map<Rank, Long> rankGroup,List<Card> cards, long count) {
 	    return cards.stream().filter(c -> rankGroup.get(c.rank()) == count).toList();
