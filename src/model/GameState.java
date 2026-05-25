cc4188458750b9787cafb49c48750f671e02210c
Thomas MAROTTA <thomasmarotta692@gmail.com>
2026-05-25 09:20:20 +0200

    Fix game state

--- src/model/GameState.java
+++ src/model/GameState.java
@@ -4,19 +4,63 @@ import java.util.Objects;
 
 import domain.Blind;
 
-public record GameState ( Blind currentBlind, int gameScore, int handsLeft) {
+public class GameState {
+	private Blind currentBlind;
+	private int gameScore;
+	private int handsLeft;
 	
-	public GameState{
+	public GameState(Blind currentBlind, int gameScore, int handsLeft) {
 		Objects.requireNonNull(currentBlind);
+		
 		if(gameScore < 0) {
 			throw new IllegalArgumentException("gameScore cannot be negative !");
 		}
 		if(handsLeft < 0) {
 			throw new IllegalArgumentException("handsLeft cannot be negative !");
 		}
+		
+		this.currentBlind = currentBlind;
+		this.gameScore = gameScore;
+		this.handsLeft = handsLeft;
+	}
+	
+	/* Getters */
+	
+	public Blind currentBlind() {
+		return currentBlind;
+	}
+	
+	public int gameScore() {
+		return gameScore;
+	}
+	
+	public int handsLeft() {
+		return handsLeft;
+	}
+	
+	/* Methods */
+	
+	public void incrementScore(int scoreToHad) {
+		gameScore += scoreToHad;
+	}
+	
+	public void resetScore() {
+		gameScore = 0;
+	}
+	
+	public void resetHandsLeft(int hands) {
+		handsLeft = hands;
+	}
+	
+	public void decrementHands() {
+		handsLeft--;
+	}
+	
+	public void changeBlind(Blind newBlind) {
+		currentBlind = newBlind;
 	}
 	
 	public boolean isBlindBeaten() {
-		return this.gameScore >= currentBlind.targetScore();
+		return gameScore >= currentBlind.targetScore();
 	}
 }
